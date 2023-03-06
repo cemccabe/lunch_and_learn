e6f82a0d7f6a043325af0839b5c21d85488dd9a5
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-06 14:34:38 -0700

    Add expectations to PlacesService test

--- spec/services/places_service_spec.rb
+++ spec/services/places_service_spec.rb
@@ -9,6 +9,26 @@ RSpec.describe PlacesService do
 
       expect(response).to have_key(:type)
       expect(response[:type]).to be_a(String)
+
+      expect(response).to have_key(:features)
+      expect(response[:features]).to be_an(Array)
+
+      feature = response[:features].first
+
+      expect(feature).to have_key(:properties)
+      expect(feature[:properties]).to be_a(Hash)
+
+      expect(feature[:properties]).to have_key(:name)
+      expect(feature[:properties][:name]).to be_a(String)
+
+      expect(feature[:properties]).to have_key(:address_line1)
+      expect(feature[:properties][:address_line1]).to be_a(String)
+
+      expect(feature[:properties]).to have_key(:address_line2)
+      expect(feature[:properties][:address_line2]).to be_a(String)
+
+      expect(feature[:properties]).to have_key(:place_id)
+      expect(feature[:properties][:place_id]).to be_a(String)
     end
   end
 end
