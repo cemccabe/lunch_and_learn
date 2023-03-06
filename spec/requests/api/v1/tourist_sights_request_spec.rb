497058a7ecacf87ab03e67e04165a9aa392c2d6f
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-06 14:55:33 -0700

    Add expectations to requests spec, pass test

--- spec/requests/api/v1/tourist_sights_request_spec.rb
+++ spec/requests/api/v1/tourist_sights_request_spec.rb
@@ -5,6 +5,17 @@ describe 'Tourist sights request', type: :request do
     get '/api/v1/tourist_sights?country=France'
     expect(response).to be_successful
     
-    tourist_sights = JSON.parse(response.body, symbolize_names: true)
+    tourist_sights = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(tourist_sights).to be_an(Array)
+    
+    tourist_sights.each do |sight|
+      expect(sight[:id]).to eq(nil)
+      expect(sight[:type]).to eq('tourist_sight')
+      expect(sight[:attributes]).to be_a(Hash)
+      expect(sight[:attributes][:name]).to be_a(String)
+      expect(sight[:attributes][:address]).to be_a(String)
+      expect(sight[:attributes][:place_id]).to be_a(String)
+    end
   end
 end
