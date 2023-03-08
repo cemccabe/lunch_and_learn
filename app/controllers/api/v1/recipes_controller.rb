49accc9566d1c66a33afc459f6df75d8cb15bae3
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-07 21:06:09 -0700

    Update RecipesController index action, pass test with country

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class RecipesController < ApplicationController
       def index
-        
+        recipes = RecipesFacade.recipes(params[:country])
+        render json: RecipeSerializer.new(recipes)
       end
     end
   end
