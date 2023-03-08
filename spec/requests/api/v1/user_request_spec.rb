158a3fe2e43ecd71708a63a1ac65c09b62acc8e5
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-08 04:44:41 -0700

    Add to user request spec expectations

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -6,10 +6,36 @@ RSpec.describe 'User requests' do
     expect(response).to be_successful
     expect(response.status).to eq(201)
 
+    user_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user_response).to be_a(Hash)
+
+    expect(user_response).to have_key(:data)
+    expect(user_response[:data]).to be_a(Hash)
+
+    expect(user_response[:data]).to have_key(:id)
+    expect(user_response[:data][:id]).to be_a(String)
+
+    expect(user_response[:data]).to have_key(:type)
+    expect(user_response[:data][:type]).to eq('user')
+
+    expect(user_response[:data]).to have_key(:attributes)
+    expect(user_response[:data][:attributes]).to be_a(Hash)
+
+    expect(user_response[:data][:attributes]).to have_key(:name)
+    expect(user_response[:data][:attributes][:name]).to be_a(String)
+
+    expect(user_response[:data][:attributes]).to have_key(:email)
+    expect(user_response[:data][:attributes][:email]).to be_a(String)
+
+    expect(user_response[:data][:attributes]).to have_key(:api_key)
+    expect(user_response[:data][:attributes][:api_key]).to be_a(String)
+
+
     user = User.last
-    expect(user.name).to eq('Test Name')
-    expect(user.email).to eq('test@gmail.com')
-    expect(user.api_key).to be_a(String)
-    expect(user.api_key.chars.count).to eq(32)
+
+    expect(user.name).to eq(user_response[:data][:attributes][:name])
+    expect(user.email).to eq(user_response[:data][:attributes][:email])
+    expect(user.api_key).to eq(user_response[:data][:attributes][:api_key])
   end
 end
