41fe331c36111fdaecb12646fb9043b2d81c4043
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-08 04:32:23 -0700

    Update UserController, add expectations in user request spec, pass test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,18 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
+        user = User.new(user_params)
+        user.create_api_key
 
+        if user.save
+          render json: UserSerializer.new(user), status: 201
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email)
       end
     end
   end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe 'User requests' do
     post '/api/v1/users', params: {user: {name: 'Test Name', email: 'test@gmail.com'}}
     expect(response).to be_successful
     expect(response.status).to eq(201)
+
+    user = User.last
+    expect(user.name).to eq('Test Name')
+    expect(user.email).to eq('test@gmail.com')
+    expect(user.api_key).to be_a(String)
+    expect(user.api_key.chars.count).to eq(32)
   end
 end
