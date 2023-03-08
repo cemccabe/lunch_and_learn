25f0765a749953d65a6a6bd311681b4d98ef4598
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-08 05:20:26 -0700

    Create Favorite model, modify User model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :api_key
   validates :email, uniqueness: true
   validates :api_key, uniqueness: true
+  has_many :favorites
 
   def create_api_key
     self.api_key = SecureRandom.hex(16)
